ec4755b2a59fd9f3fe396c202c72cab97bb91d6b
MorveN11 <morales.patty.jose@gmail.com>
2022-12-14 03:38:15 -0400

    Add uuid

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,11 +1,10 @@
 import pool from '../utils/db.js';
 import { encrypt } from '../utils/encript.js';
-import { v4 as uuid } from 'uuid';
 
 export const postAdmin = async (req, res) => {
   try {
     const admin = {
-      id_admin: uuid(),
+      id_admin: req.body.id_admin,
       email: req.body.email,
       password: encrypt(req.body.password)
     };
